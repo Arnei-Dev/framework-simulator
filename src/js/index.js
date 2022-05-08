fc02f1dc724ea63a157aa4698aee29380c264ab2
Arnei-Dev <mr.arneydias@gmail.com>
2022-05-08 12:08:25 -0300

    simulator model

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import '../scss/styles.scss'
+import imageRiver from '../assets/jackery-power-01.jpg'
+import imageNotebook from '../assets/jackery-power-02.jpg'
+
+const elemRoot = document.getElementById('root')
+elemRoot.classList.add('container')
+
+
+const elemImageRiver = new Image()
+elemImageRiver.src = imageRiver
+
+const elemImageNotebook = new Image()
+elemImageNotebook.src = imageNotebook
+
+document.querySelector('#river')
+.appendChild(elemImageRiver)
+
+document.querySelector('#notebook')
+.appendChild(elemImageNotebook)
